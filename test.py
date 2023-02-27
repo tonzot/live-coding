25fcc05d7883fabfd52ea7b6fab3608fa0bc1c3f
Haydar Genc <haydar.genc@rwth-aachen.de>
2023-02-27 18:39:02 +0100

    Create test.py file

---
+++ test.py
@@ -0,0 +1,9 @@
+# Arguments on first line forbidden when not using vertical alignment.
+foo = long_function_name(var_one, var_two,
+    var_three, var_four)
+
+# Further indentation required as indentation is not distinguishable.
+def long_function_name(
+    var_one, var_two, var_three,
+    var_four):
+    print(var_one)
